0f22dbd67b7a613826ae713aa928d3b874da9550
Haikal Alqardhawy <haikalalqardhawy@gmail.com>
2025-08-06 22:18:02 +0700

    Update config.php

--- login/php/config.php
+++ login/php/config.php
@@ -1,11 +1,8 @@
 <?php
-// Path database SQLite
 $db_path = __DIR__ . '/../db/database.sqlite';
 
-// Buat koneksi
 $conn = new SQLite3($db_path);
 
-// Buat tabel users jika belum ada
 $conn->exec("CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL,
